bc18b97b150deff7b6deb71ac1e266d3f3527296
Iram.Ashfaq <161501174+iram-ashfaq@users.noreply.github.com>
2024-03-29 22:21:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+//Questions number 11
+var names = ["adiba", "sabeel", "sabahat", "reeba", "bushra", "nimra"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
+console.log(names[5]);
+//Question number 12
+var mynames = ["adiba", "sabeel", "sabahat", "reeba", "bushra", "nimra"];
+var message = "how are you?";
+console.log(mynames[0] + " " + message);
+console.log(mynames[1] + " " + message);
+console.log(mynames[2] + " " + message);
+console.log(mynames[3] + " " + message);
+console.log(mynames[4] + " " + message);
+console.log(mynames[5] + " " + message);
+//Question number 13
+var transportation = ["AUDI", "BMW", "HOUNDA CITY"];
+transportation.map(function (items) { return console.log("i would like to own a ".concat(items)); });
---
+++ main.ts
@@ -0,0 +1,25 @@
+//Questions number 11
+let names : string [] = ["adiba","sabeel","sabahat","reeba","bushra","nimra"];
+console.log(names[0]);
+console.log(names[1])
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
+console.log(names[5]);
+
+//Question number 12
+let mynames : string [] = ["adiba","sabeel","sabahat","reeba","bushra","nimra"];
+let message : string = "how are you?"
+console.log(mynames[0] + " "+ message )
+console.log(mynames[1] + " "+ message )
+console.log(mynames[2] + " "+ message )
+console.log(mynames[3] + " "+ message )
+console.log(mynames[4] + " "+ message )
+console.log(mynames[5] + " "+ message )
+
+//Question number 13
+let transportation : string  [] =["AUDI","BMW","HOUNDA CITY"];
+transportation.map((items) => console.log(`i would like to own a ${items}`));
+
+
+
